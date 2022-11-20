cf2c9d46d501c83b9a178319f1becee50bddfde9
Dania <Dania2008@list.ru>
2022-11-20 13:09:45 +0300

    Filling first array witn data and getting size of final array

--- Program.cs
+++ Program.cs
@@ -6,4 +6,11 @@ Console.Clear();
 Console.WriteLine("Введите количество элементов в массиве: ");
 int n = Convert.ToInt32(Console.ReadLine());
 string[]text = new string[n];
-
+int m = 0;
+for(int i = 0; i < text.Length; i++)
+{
+    Console.WriteLine("Введите элемент массива: ");
+    text[i] = Console.ReadLine();
+    if(text[i].Length < 4)
+        m++;
+}
